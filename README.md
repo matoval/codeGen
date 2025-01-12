7bd0da1f4620a40a4c7ca67b2ed3d2dec48f58f3
matoval <mat24c@gmail.com>
2025-01-11 19:44:00 -0800

    Create docker container as a code sandbox

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # codeGen
+Generate code using a local small model like llama3.2 3b. Write tests first then use tests to verify that the code works.
+
+## architecture
+LLM - local llm running on current system or another system over network.
+Code sandbox - docker container to have the llm run live code.
---
+++ internal/codeSandbox/codeSandbox.go
@@ -0,0 +1,60 @@
+package codesandbox
+
+import (
+	"context"
+	"io"
+	"os"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+)
+
+var ContainerID string
+
+func Init() {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	defer cli.Close()
+
+	reader, err := cli.ImagePull(ctx, "debian:bullseye-slim", image.PullOptions{})
+	if err != nil {
+		panic(err)
+	}
+	defer reader.Close()
+
+	io.Copy(os.Stdout, reader)
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "debian:bullseye-slim",
+	}, nil, nil, nil, "")
+	if err != nil {
+		panic(err)
+	}
+
+	ContainerID = resp.ID
+
+	if err := cli.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
+		panic(err)
+	}
+
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			panic(err)
+		}
+	case <-statusCh:
+	}
+
+	out, err := cli.ContainerLogs(ctx, resp.ID, container.LogsOptions{ShowStdout: true})
+	if err != nil {
+		panic(err)
+	}
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+}
